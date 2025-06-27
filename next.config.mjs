b7f458c5ce3f60ec155fbbf49ca9ccbbe6fb582e
NandiniBasu05 <nandinibasu240105@gmail.com>
2025-06-27 16:55:02 +0530

    Fixed image display for testimonials

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "randomuser.me",
+                 pathname: "/api/portraits/**",
             }
         ]
     },
